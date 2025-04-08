85c40b04b6074b62a0d0fc7adfb1569a070bfe81
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-08 17:11:27 +0000

    add-authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://gorgeous-camel-74.clerk.accounts.dev/",
+      applicationId: "convex",
+    },
+  ],
+};
--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
@@ -12,6 +13,7 @@ export const Navbar = () => {
         <h3 className="text-xl">Docs</h3>
       </div>
       <SearchInput />
+      <UserButton />
       <div />
     </nav>
   );
