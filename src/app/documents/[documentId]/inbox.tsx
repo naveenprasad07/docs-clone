8d2d729ca3091ace160eee3ae151ebc94f94f299
Naveen Prasad <128902289+naveenprasad07@users.noreply.github.com>
2025-04-10 11:54:50 +0000

    add functionalities

--- src/app/documents/[documentId]/inbox.tsx
+++ src/app/documents/[documentId]/inbox.tsx
@@ -1,1 +1,69 @@
-    
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
+import {
+  ClientSideSuspense,
+  useInboxNotifications,
+} from "@liveblocks/react/suspense";
+import { Separator } from "@/components/ui/separator";
+import { BellIcon } from "lucide-react";
+
+export const Inbox = () => {
+  return (
+    <>
+      <ClientSideSuspense
+        fallback={
+          <Button variant={"ghost"} disabled className="relative" size="icon">
+            <BellIcon className="size-5" />
+          </Button>
+        }
+      >
+        <InboxMenu />
+      </ClientSideSuspense>
+      <Separator orientation="vertical" className="h-6" />
+    </>
+  );
+};
+
+const InboxMenu = () => {
+  const { inboxNotifications } = useInboxNotifications();
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} className="relative" size="icon">
+            <BellIcon className="size-5" />
+            {inboxNotifications?.length > 0 && (
+              <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
+                {inboxNotifications.length}
+              </span>
+            )}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-auto">
+          {inboxNotifications.length > 0 ? (
+            <InboxNotificationList>
+              {inboxNotifications.map((inboxNotification) => (
+                <InboxNotification
+                  key={inboxNotification.id}
+                  inboxNotification={inboxNotification}
+                />
+              ))}
+            </InboxNotificationList>
+          ) : (
+            <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
+              No Notifications
+            </div>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Separator orientation="vertical" className="h-6" />
+    </>
+  );
+};
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -38,6 +38,7 @@ import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 export const Navbar = () => {
   const { editor } = useEditorStore();
@@ -257,6 +258,7 @@ export const Navbar = () => {
       </div>
       <div className="flex gap-3 items-center pl-6">
         <Avatars />
+        <Inbox />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
